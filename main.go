f17081c8b0a4a31ace8f7c2c3bc192942b70fad6
giwa <ugw.gi.world@gmail.com>
2016-04-15 19:24:22 +0900

    fix function for starting server

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/labstack/echo/engine/standard"
 	"github.com/westlab/door-api/route"
 )
 
 func main() {
 	router := route.Init()
-	router.Run(":8080")
+	router.Run(standard.New(":8080"))
 }
