dbbb150273a3efe6a44cea4c1d037d3dadcd3802
giwa <ugw.gi.world@gmail.com>
2016-05-08 20:18:59 +0900

    Update  dpi and http communicaiton model for reconstructing http

--- model/http_communication.go
+++ model/http_communication.go
@@ -6,14 +6,15 @@ import (
 
 // HTTPCommunication is generated HTTP request and response
 type HTTPCommunication struct {
-	SrcIP   string
-	DstIP   string
-	SrcPort int64
-	DstPort int64
-	Host    string
-	URI     string
-	Title   string
-	Time    time.Time
+	SrcIP       string
+	DstIP       string
+	SrcPort     int64
+	DstPort     int64
+	Host        string
+	URI         string
+	Title       string
+	ContentType string
+	Time        time.Time
 }
 
 // URL return URL generated from Host and URI
@@ -22,4 +23,20 @@ func (h *HTTPCommunication) URL() string {
 		return h.Host + h.URI
 	}
 	return ""
+}
+
+// IsValid validate http communication
+// TODO: reconsider interface
+func (h *HTTPCommunication) IsValid(contentType string) bool {
+	if contentType == "" {
+		contentType = "text/html"
+	}
+
+	if h.SrcIP != "" && h.DstIP != "" &&
+		h.SrcPort != 0 && h.DstPort != 0 &&
+		h.URL() != "" && h.Title != "" &&
+		h.ContentType == contentType {
+		return true
+	}
+	return false
 }
--- model/http_communication_test.go
+++ model/http_communication_test.go
@@ -3,12 +3,14 @@ package model
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestURL(t *testing.T) {
 	hc := HTTPCommunication{
 		SrcIP: "1.1.1.1", DstIP: "1.1.1.2",
-		SrcPort: 1234, DstPort: 80,
+		SrcPort: 1234, DstPort: 80, Title: "foo",
 		Host: "google.com", URI: "/foo", Time: time.Now(),
 	}
 	actual := hc.URL()
@@ -16,4 +18,17 @@ func TestURL(t *testing.T) {
 	if actual != expected {
 		t.Errorf("got %v\n want %v", actual, expected)
 	}
+}
+
+func TestIsValid(t *testing.T) {
+	hc := HTTPCommunication{
+		SrcIP: "", DstIP: "1.1.1.2",
+		SrcPort: 1234, DstPort: 80, Title: "foo",
+		Host: "google.com", URI: "/foo", Time: time.Now(),
+	}
+	assert.Equal(t, false, hc.IsValid(""))
+	hc.SrcIP = "1.1.1.1"
+	assert.Equal(t, false, hc.IsValid(""))
+	hc.ContentType = "text/html"
+	assert.Equal(t, true, hc.IsValid(""))
 }
