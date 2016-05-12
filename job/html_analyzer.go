bb05e348c701e9fe650cc6c0f0680208e511a523
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-05-12 20:56:16 +0900

    add GetNouns and WordCount to html_analyzer

--- job/html_analyzer.go
+++ job/html_analyzer.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/westlab/door-api/model"
+
+	"github.com/ikawaha/kagome/tokenizer"
 	"github.com/kennygrant/sanitize"
 )
 
@@ -29,4 +32,39 @@ func DonwloadHTML(url string) (html string, err error) {
 func RemoveHTMLTags(html string) string {
 	html = sanitize.HTML(html)
 	return html
+}
+
+// GetNouns gets nouns from text
+func GetNouns(text string) (words []string) {
+	t := tokenizer.New()
+	tokens := t.Tokenize(text)
+
+	for _, token := range tokens {
+		if token.Class == tokenizer.DUMMY {
+			continue
+		}
+		if token.Pos() == "名詞" {
+			words = append(words, token.Surface)
+		}
+	}
+	return words
+}
+
+// WordCount counts words from words slice
+func WordCount(words []string) (counts []model.Count) {
+	wordmap := make(map[string]int64)
+	for _, word := range words {
+		if _, ok := wordmap[word]; ok {
+			wordmap[word]++
+		} else {
+			wordmap[word] = 1
+		}
+	}
+
+	// format wordmap to counts
+	for key, value := range wordmap {
+		counts = append(counts, model.Count{Name: key, Count: value})
+	}
+
+	return counts
 }
