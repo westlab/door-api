79b4b1d6b2a588f772df4461ddc5676d1deeb363
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-05-21 00:31:11 +0900

    fix model tests to use testify/suite

--- model/browsing_test.go
+++ model/browsing_test.go
@@ -1,20 +1,69 @@
 package model
 
 import (
+	"database/sql"
 	"log"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/gchaincl/dotsql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/suite"
+	"github.com/westlab/door-api/conf"
+	"github.com/westlab/door-api/context"
 )
 
-func TestBrowsingCRUD(t *testing.T) {
+type BrowsingTestSuite struct {
+	suite.Suite
+}
+
+func TestBrowsingTestSuite(t *testing.T) {
+	suite.Run(t, new(BrowsingTestSuite))
+}
+
+func (s *BrowsingTestSuite) SetupTest() {
+	// load conf file
+	conf := conf.New("../config.toml")
+	context.NewContext(conf)
+	cxt := context.GetContext()
+
+	db, err := sql.Open(cxt.GetConf().DBType, cxt.GetConf().GetDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dsql, err := dotsql.LoadFromFile("../test/test_data.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = dsql.Exec(db, "drop-browsing-table")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = dsql.Exec(db, "create-browsing-table")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = dsql.Exec(db, "insert-browsing-rows")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *BrowsingTestSuite) TearDonwTest() {
+}
+
+func (s *BrowsingTestSuite) TestBrowsingCRUD() {
 	// Save
 	b := NewBrowsing("10.24.1.20", "6.6.6.6", 123, 80, 3, 20, "Mitsubishi", "http://mitsubishi.co.jp", "mitsubishi.co.jp", time.Now())
 	b.Save()
 	bs := GetBrowsingBySrcIP("10.24.1.20")
-	assert.Equal(t, "Mitsubishi", bs[0].Title)
+	s.Equal("Mitsubishi", bs[0].Title)
 
 	// Update
 	b = GetBrowsingByID(int64(2))
@@ -24,28 +73,28 @@ func TestBrowsingCRUD(t *testing.T) {
 		log.Println(err)
 	}
 	b = GetBrowsingByID(int64(2))
-	assert.Equal(t, "KiraYoshikage", b.Title)
+	s.Equal("KiraYoshikage", b.Title)
 
 	// Delete
 	// TODO: add delete test
 }
 
-func TestBrowsingJSON(t *testing.T) {
+func (s *BrowsingTestSuite) TestBrowsingJSON() {
 }
 
-func TestGetBrowsingByID(t *testing.T) {
+func (s *BrowsingTestSuite) TestGetBrowsingByID() {
 }
 
-func TestGetBrowsingBySrcIP(t *testing.T) {
+func (s *BrowsingTestSuite) TestGetBrowsingBySrcIP() {
 }
 
-func TestGetBrowsings(t *testing.T) {
+func (s *BrowsingTestSuite) TestGetBrowsings() {
 }
 
-func TestGetBrowsingHistogram(t *testing.T) {
+func (s *BrowsingTestSuite) TestGetBrowsingHistogram() {
 }
 
-func TestMakeCountCase(t *testing.T) {
+func (s *BrowsingTestSuite) TestMakeCountCase() {
 	start := time.Date(1990, time.November, 24, 0, 0, 0, 0, time.UTC)
 	end := start.Add(time.Duration(100) * time.Minute)
 	windows := makeHistogramWindow(start, end, int64(10))
@@ -60,19 +109,19 @@ COUNT(CASE WHEN timestamp >= '1990-11-24 00:50:00' AND timestamp < '1990-11-24 0
 COUNT(CASE WHEN timestamp >= '1990-11-24 01:00:00' AND timestamp < '1990-11-24 01:10:00' THEN 1 END) AS '1:00',
 COUNT(CASE WHEN timestamp >= '1990-11-24 01:10:00' AND timestamp < '1990-11-24 01:20:00' THEN 1 END) AS '1:10',
 COUNT(CASE WHEN timestamp >= '1990-11-24 01:20:00' AND timestamp < '1990-11-24 01:30:00' THEN 1 END) AS '1:20'`
-	assert.Equal(t, expected, c)
+	s.Equal(expected, c)
 }
 
-func TestMakeHistogramWindow(t *testing.T) {
+func (s *BrowsingTestSuite) TestMakeHistogramWindow() {
 	start := time.Date(1990, time.November, 24, 0, 0, 0, 0, time.UTC)
 	end := start.Add(time.Duration(100) * time.Minute)
 	windows := makeHistogramWindow(start, end, int64(10))
-	assert.Equal(t, 9, len(windows))
-	assert.Equal(t, windows[0].Y, windows[1].X)
-	assert.Equal(t, windows[7].Y, windows[8].X)
+	s.Equal(9, len(windows))
+	s.Equal(windows[0].Y, windows[1].X)
+	s.Equal(windows[7].Y, windows[8].X)
 }
 
-func TestMakeUnpivotHistogram(t *testing.T) {
+func (s *BrowsingTestSuite) TestMakeUnpivotHistogram() {
 	start := time.Date(1990, time.November, 24, 0, 0, 0, 0, time.UTC)
 	end := start.Add(time.Duration(100) * time.Minute)
 	windows := makeHistogramWindow(start, end, int64(10))
@@ -96,11 +145,11 @@ func TestMakeUnpivotHistogram(t *testing.T) {
 		"UNION ALL",
 		"SELECT '1:20' AS name, `browsing_histogram`.`1:20` FROM `browsing_histogram`"}
 	expected := strings.Join(unions, "\n")
-	assert.Equal(t, expected, unp)
+	s.Equal(expected, unp)
 }
 
-func TestGetBrowsingRank(t *testing.T) {
+func (s *BrowsingTestSuite) TestGetBrowsingRank() {
 }
 
-func TestGetBrowsingAfterID(t *testing.T) {
+func (s *BrowsingTestSuite) TestGetBrowsingAfterID() {
 }
