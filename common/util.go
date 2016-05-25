85af670cd83d82bafab3ea8a0b241d9d5e66b46b
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-05-26 03:00:31 +0900

    fix HTML analyzer job and add related tests

--- common/util.go
+++ common/util.go
@@ -1,9 +1,12 @@
 package common
 
 import (
+	"crypto/md5"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/hex"
 	"log"
+	"os"
 )
 
 // RandomStr generate random string
@@ -18,4 +21,18 @@ func RandomStr() string {
 	}
 
 	return base64.URLEncoding.EncodeToString(rb)
+}
+
+// GetMD5Hash gets hash string from a string
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// IsFileExist checks if the file name is existed
+func IsFileExist(fileName string, dirPath string) bool {
+	fpath := dirPath + "/" + fileName
+	_, err := os.Stat(fpath)
+	return err == nil
 }
